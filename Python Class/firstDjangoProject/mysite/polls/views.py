74e334f5738e30f6872d6ecda2cab31d82e47e95
Sobowale-Olayiwola <layitheinfotechguru@gmail.com>
2021-05-04 23:50:25 +0100

    First Django App

---
+++ Python Class/firstDjangoProject/mysite/polls/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, fellow pythonistas in Zuri. Nice to meet everyone of you")
